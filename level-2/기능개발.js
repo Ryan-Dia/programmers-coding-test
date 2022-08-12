883a7dfdb3ce371a6ddbebe18f6297898424429e
Fe1 <76567238+RyanDeclan@users.noreply.github.com>
2022-08-12 20:39:08 +0900

    Update 기능개발.js

--- level-2/기능개발.js
+++ level-2/기능개발.js
@@ -114,3 +114,37 @@ function solution(progresses, speeds) {
   // 정답 반환
   return answer;
 }
+
+// 정답 6 - RyanDeclan
+// forEach , map , filter 보다는 속도가 더 빠른 for문을 사용
+function solution(progresses, speeds) {
+    // 각각의 progress 의 기존 인덱스와 해당 기능의 작업한 n일을 포함하는 배열
+    let countBox = [];
+    let completeBox = [];
+    for(k=1; k<100; k++){
+        let complete = []
+        for(i=0; i<progresses.length; i++){
+            if(typeof(progresses[i]) == "number") progresses[i] += speeds[i]
+            if(progresses[i] >= 100){
+                complete.push([i,k]);
+                progresses.splice(i,1,"end")
+            } 
+        }
+        if(complete[0] != null)completeBox.push(...complete)
+    } 
+    completeBox.sort((a,b) => a[0] - b[0])
+    
+    // 인덱스와 작업한 일수가 같이 들어가있는 배열을 가지고 count하는 작업 
+    let count = 0;
+    completeBox.reduce((acc, cur, i) => {
+        if( acc[0] < cur[1] ){
+            if(i) countBox.push(count)
+            acc[0] = cur[1];
+            count = 0;
+            count++
+        }else{count++}
+        if(completeBox.length == i + 1) countBox.push(count)
+        return acc
+    } ,[0])
+    return countBox;
+}
